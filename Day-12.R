70403f148288bff4233d0f7d1fc295a29a40c187
Shobika-Gaddam <90687734+Shobika-Gaddam@users.noreply.github.com>
2021-11-30 11:20:24 +0530

    Add files via upload

---
+++ Day-12.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(ggplot2)
+library(readr)
+library(maps)
+library(viridis)
+library(scales)
+
+datacov <- read_csv("C:/Users/shobi/OneDrive/Desktop/Woxsen/Term 2/R Programming/covid19.csv.csv")
+
+#get the world map
+world<-map_data("world")
+mybreaks<-c(1,20,100,1000,50000)
+
+ggplot() +
+  geom_polygon(data = world, aes(x=long, y = lat, group = group), fill="grey", alpha=0.3) +
+  geom_point(data=datacov, aes(x=Long, y=Lat, size=`3/3/20`, color=`3/3/20`),stroke=F, alpha=0.7) +
+  scale_size_continuous(name="Cases", trans="log", range=c(1,7),breaks=mybreaks, labels = c("1-19", "20-99", "100-999", "1,000-49,999", "50,000+")) +
+  # scale_alpha_continuous(name="Cases", trans="log", range=c(0.1, 0.9),breaks=mybreaks) +
+  scale_color_viridis_c(option="inferno",name="Cases", trans="log",breaks=mybreaks, labels = c("1-19", "20-99", "100-999", "1,000-49,999", "50,000+")) +
+  theme_void() + 
+  guides( colour = guide_legend()) +
+  labs(caption = "Data Repository provided by Johns Hopkins CSSE. Visualization by DataScience Students of Woxsen University") +
+  theme(
+    legend.position = "bottom",
+    text = element_text(color = "#22211d"),
+    plot.background = element_rect(fill = "#ffffff", color = NA), 
+    panel.background = element_rect(fill = "#ffffff", color = NA), 
+    legend.background = element_rect(fill = "#ffffff", color = NA)
+  )
+
+
